ac0b81bb9c03831246559d5b0edbbbd86b9b4717
JuanRiggio <jcriggio1@gmail.com>
2025-07-24 18:33:30 -0300

    PASE VENDEDORES Y CLIENTES

---
+++ Visual/Vendedor/AgregarSectorEvento.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace Visual.Vendedor
+{
+    partial class AgregarSectorEvento
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.btnregistrarse = new System.Windows.Forms.Button();
+            this.txtCapacidad = new System.Windows.Forms.TextBox();
+            this.lblmail = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblyatengocuen = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(117, 150);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 20);
+            this.label5.TabIndex = 50;
+            this.label5.Text = "Precio";
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(231)))), ((int)(((byte)(233)))));
+            this.txtPrecio.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtPrecio.Font = new System.Drawing.Font("MS Gothic", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPrecio.Location = new System.Drawing.Point(117, 186);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(284, 32);
+            this.txtPrecio.TabIndex = 51;
+            // 
+            // btnregistrarse
+            // 
+            this.btnregistrarse.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(116)))), ((int)(((byte)(86)))), ((int)(((byte)(174)))));
+            this.btnregistrarse.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnregistrarse.FlatAppearance.BorderSize = 0;
+            this.btnregistrarse.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnregistrarse.ForeColor = System.Drawing.Color.White;
+            this.btnregistrarse.Location = new System.Drawing.Point(117, 299);
+            this.btnregistrarse.Margin = new System.Windows.Forms.Padding(6, 3, 6, 3);
+            this.btnregistrarse.Name = "btnregistrarse";
+            this.btnregistrarse.Size = new System.Drawing.Size(284, 53);
+            this.btnregistrarse.TabIndex = 48;
+            this.btnregistrarse.Text = "REGISTRARSE";
+            this.btnregistrarse.UseVisualStyleBackColor = false;
+            // 
+            // txtCapacidad
+            // 
+            this.txtCapacidad.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(231)))), ((int)(((byte)(233)))));
+            this.txtCapacidad.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtCapacidad.Font = new System.Drawing.Font("MS Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCapacidad.Location = new System.Drawing.Point(117, 266);
+            this.txtCapacidad.Margin = new System.Windows.Forms.Padding(6, 3, 6, 3);
+            this.txtCapacidad.Name = "txtCapacidad";
+            this.txtCapacidad.Size = new System.Drawing.Size(284, 32);
+            this.txtCapacidad.TabIndex = 46;
+            // 
+            // lblmail
+            // 
+            this.lblmail.AutoSize = true;
+            this.lblmail.Location = new System.Drawing.Point(117, 230);
+            this.lblmail.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblmail.Name = "lblmail";
+            this.lblmail.Size = new System.Drawing.Size(85, 20);
+            this.lblmail.TabIndex = 45;
+            this.lblmail.Text = "Capacidad";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(117, 83);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(65, 20);
+            this.label6.TabIndex = 44;
+            this.label6.Text = "Nombre";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(231)))), ((int)(((byte)(233)))));
+            this.txtNombre.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtNombre.Font = new System.Drawing.Font("MS Gothic", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNombre.Location = new System.Drawing.Point(117, 119);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(284, 32);
+            this.txtNombre.TabIndex = 49;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Nirmala UI", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(116)))), ((int)(((byte)(86)))), ((int)(((byte)(174)))));
+            this.label1.Location = new System.Drawing.Point(12, 26);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(435, 60);
+            this.label1.TabIndex = 38;
+            this.label1.Text = "REGISTRAR SECTOR";
+            // 
+            // lblyatengocuen
+            // 
+            this.lblyatengocuen.AutoSize = true;
+            this.lblyatengocuen.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lblyatengocuen.Font = new System.Drawing.Font("Nirmala UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblyatengocuen.Location = new System.Drawing.Point(208, 636);
+            this.lblyatengocuen.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblyatengocuen.Name = "lblyatengocuen";
+            this.lblyatengocuen.Size = new System.Drawing.Size(66, 25);
+            this.lblyatengocuen.TabIndex = 47;
+            this.lblyatengocuen.Text = "Volver";
+            // 
+            // AgregarSectorEvento
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(508, 694);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtPrecio);
+            this.Controls.Add(this.btnregistrarse);
+            this.Controls.Add(this.lblyatengocuen);
+            this.Controls.Add(this.txtCapacidad);
+            this.Controls.Add(this.lblmail);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label1);
+            this.Name = "AgregarSectorEvento";
+            this.Text = "AgregarSectorEvento";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.Button btnregistrarse;
+        private System.Windows.Forms.TextBox txtCapacidad;
+        private System.Windows.Forms.Label lblmail;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblyatengocuen;
+    }
+}
